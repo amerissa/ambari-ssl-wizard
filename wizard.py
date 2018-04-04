0644fea0bcdccd71d96df92a5df236c2acf43962
Amer Issa <amer.issa1@gmail.com>
2018-04-04 17:22:44 -0400

    create readme and fix formatting to standard python

--- wizard.py
+++ wizard.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python27
+#!/usr/bin/env python2.7
 
 import requests
 import json
@@ -16,31 +16,35 @@ class propertiesupdater(object):
     def __init__(self, definitions):
         self.ambari = ambariProps(protocol, host, port, username, password, clustername)
         self.definitions = definitions
+
     def service(self, service):
         for site, props in self.definitions[service].iteritems():
                 for prop, value in props.iteritems():
                     finalvalue = self.replacefunc(value)
                     execute = self.ambari.set(site, prop, finalvalue)
                     print(execute)
+
     def replacefunc(self, value):
         for key, prop in changeprops.iteritems():
             value = value.replace(key, prop)
         return(value)
 
+
 class ambariProps(object):
     def __init__(self, protocol, host, port, username, password, clustername):
         self.command = '/var/lib/ambari-server/resources/scripts/configs.py -p %s -t %s -l %s -u %s -s %s -n %s ' % (password, port, host, username, protocol, clustername)
+
     def get(self, config, property):
         command = '%s -c %s -a %s' % (self.command, config, 'get')
         data = os.popen(command + '| grep -v "###"').read()
         try:
             props = json.loads(data)
         except:
-            final='http://localhost:8080'
+            final = 'http://localhost:8080'
         try:
             result = props["properties"][property]
         except:
-            final='http://localhost:8080'
+            final = 'http://localhost:8080'
         try:
             final
         except:
@@ -49,10 +53,12 @@ class ambariProps(object):
             return(result)
         else:
             return(final)
+
     def set(self, config, property, value):
         command = "%s -c %s -a %s -k %s -v '%s'" % (self.command, config, 'set', property, value)
         result = os.popen(command).read()
         return(result)
+
 
 def ambariREST(protocol, host, port, username, password, endpoint):
     url = protocol + "://" + host + ":" + str(port) + "/" + endpoint
@@ -63,6 +69,7 @@ def ambariREST(protocol, host, port, username, password, endpoint):
         sys.exit(1)
     return(json.loads(r.text))
 
+
 def loaddefinitions():
     try:
         definitions = json.loads(open("./definitions.json").read())
@@ -70,6 +77,7 @@ def loaddefinitions():
         print("Cannot read defintions file")
         sys.exit(1)
     return(definitions)
+
 
 def replaceurl(url, port):
     parsed = urlparse(url)
@@ -79,12 +87,12 @@ def replaceurl(url, port):
 
 def main():
     parser = optparse.OptionParser(usage="usage: %prog [options]")
-    parser.add_option("-S", "--protocol", dest="protocol", default="http", help="default is http, set to https if required" )
-    parser.add_option("-P", "--port", dest="port", default="8080", help="Set Ambari Protocol" )
-    parser.add_option("-u", "--username", dest="username", default="admin", help="Ambari Username" )
-    parser.add_option("-p", "--password", dest="password", default="admin", help="Ambari Password" )
-    parser.add_option("-H", "--host", dest="host", default="localhost", help="Ambari Host" )
-    parser.add_option("-C", "--configfile", dest="configs", default="./configs", help="Config file containing key and truststore information" )
+    parser.add_option("-S", "--protocol", dest="protocol", default="http", help="default is http, set to https if required")
+    parser.add_option("-P", "--port", dest="port", default="8080", help="Set Ambari Protocol")
+    parser.add_option("-u", "--username", dest="username", default="admin", help="Ambari Username")
+    parser.add_option("-p", "--password", dest="password", default="admin", help="Ambari Password")
+    parser.add_option("-H", "--host", dest="host", default="localhost", help="Ambari Host")
+    parser.add_option("-C", "--configfile", dest="configs", default="./configs", help="Config file containing key and truststore information")
 
     (options, args) = parser.parse_args()
     global username
@@ -99,7 +107,7 @@ def main():
     protocol = options.protocol
     host = options.host
     clustername = ambariREST(protocol, host, port, username, password, "api/v1/clusters")["items"][0]["Clusters"]["cluster_name"]
-    installedservices = [ line["ServiceInfo"]["service_name"] for line in ambariREST(protocol, host, port, username, password, "api/v1/clusters/" + clustername + "/services" )["items"]]
+    installedservices = [line["ServiceInfo"]["service_name"] for line in ambariREST(protocol, host, port, username, password, "api/v1/clusters/" + clustername + "/services")["items"]]
     definitions = loaddefinitions()
     ambari = ambariProps(protocol, host, port, username, password, clustername)
     updater = propertiesupdater(definitions)
@@ -107,17 +115,17 @@ def main():
     Config.read(options.configs)
     global changeprops
     changeprops = {
-    "KEYSTORELOC" : Config.get("Configs", "KeyStoreLocation") + '/server.jks',
-    "KEYSTORERANGER" : Config.get("Configs", "KeyStoreLocation") + '/ranger-plugin.jks',
-    "KEYPASS" : Config.get("Configs", "KeyStorePassword"),
-    "TRUSTSTORELOC" : Config.get("Configs", "TrustStoreLocation")+ '/truststore.jks',
-    "TRUSTSTOREPASS" : Config.get("Configs", "TrustStorePassword"),
-    "RANGERCOMMONNAME" : 'ranger.' + Config.get("Configs", "Domain"),
-    "RANGERURL" : replaceurl(ambari.get("admin-properties", "policymgr_external_url"), 6182),
-    "TIMELINEURL" : ambari.get("yarn-site", "yarn.timeline-service.webapp.address").split(':')[0] + ':8190',
-    "HISTORYURL" : replaceurl('http://' + ambari.get("mapred-site", "mapreduce.jobhistory.webapp.address"), 19889),
-    "KMSURL" : str(ambari.get("core-site", "hadoop.security.key.provider.path").replace(':9292', ':9393')).replace('//http@' , '//https@'),
-    "ATLASURL" : replaceurl(ambari.get("application-properties", "atlas.rest.address" ), 21443)
+        "KEYSTORELOC": Config.get("Configs", "KeyStoreLocation") + '/server.jks',
+        "KEYSTORERANGER": Config.get("Configs", "KeyStoreLocation") + '/ranger-plugin.jks',
+        "KEYPASS": Config.get("Configs", "KeyStorePassword"),
+        "TRUSTSTORELOC": Config.get("Configs", "TrustStoreLocation") + '/truststore.jks',
+        "TRUSTSTOREPASS": Config.get("Configs", "TrustStorePassword"),
+        "RANGERCOMMONNAME": 'ranger.' + Config.get("Configs", "Domain"),
+        "RANGERURL": replaceurl(ambari.get("admin-properties", "policymgr_external_url"), 6182),
+        "TIMELINEURL": ambari.get("yarn-site", "yarn.timeline-service.webapp.address").split(':')[0] + ':8190',
+        "HISTORYURL": replaceurl('http://' + ambari.get("mapred-site", "mapreduce.jobhistory.webapp.address"), 19889),
+        "KMSURL": str(ambari.get("core-site", "hadoop.security.key.provider.path").replace(':9292', ':9393')).replace('//http@', '//https@'),
+        "ATLASURL": replaceurl(ambari.get("application-properties", "atlas.rest.address"), 21443)
     }
     for service in installedservices:
         if service in definitions.keys():
@@ -125,9 +133,10 @@ def main():
         else:
             continue
 
+
 if __name__ == "__main__":
-  try:
-    sys.exit(main())
-  except (KeyboardInterrupt, EOFError):
-    print("\nAborting ... Keyboard Interrupt.")
-    sys.exit(1)
+    try:
+        sys.exit(main())
+    except (KeyboardInterrupt, EOFError):
+        print("\nAborting ... Keyboard Interrupt.")
+        sys.exit(1)
