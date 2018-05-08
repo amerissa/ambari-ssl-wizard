4fb8871757f78f9a1913ad2634ddc5d3058b444b
Amer Issa <amer.issa1@gmail.com>
2018-05-07 20:48:10 -0400

    add ambari server enablement server

---
+++ ambari-server-ssl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z $1 ] && ! [ -s $1 ]; then
+  echo Configs file does not exist, specify as first argument
+  exit 1
+fi
+
+configs=$1
+
+KEYLOC=`cat $config | grep KeyStoreLocation | cut -d "=" -f 2`
+if [ -z $KEYLOC ]; then
+  echo KeyStoreLocation is not specified
+  exit 1
+fi
+
+TRUSTLOC=`cat $config | grep TrustStoreLocation | cut -d "=" -f 2`
+if [ -z $TRUSTLOC ]; then
+  echo TrustStoreLocation is not specified
+  exit 1
+fi
+
+TRUSTPASS=`cat $config | grep TrustStorePassword | cut -d "=" -f 2`
+if [ -z ${TRUSTPASS} ]; then
+  echo TrustStorePassword is not specified
+  exit 1
+fi
+
+ambari-server setup-security --security-option=setup-https --truststore-type=jks --truststore-path=$TRUSTLOC/truststore.jks --truststore-password=${TRUSTPASS} --import-cert-path=$KEYLOC/server.pem --import-key-path=$KEYLOC/server.key --pem-password="" --api-ssl=true --api-ssl-port=8443
+
+ambari-server setup-security --security-option=setup-truststore --truststore-type=jks --truststore-path=$TRUSTLOC/truststore.jks --truststore-password=${TRUSTPASS}
